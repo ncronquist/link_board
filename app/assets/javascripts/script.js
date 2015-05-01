b3751495fde9059b93ff2524d72f176820c3c81b
ncronquist <nick@ncronquist.com>
2015-05-01 10:35:37 -0700

    Update js to correctly change vote button classes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -9,6 +9,7 @@ $(function() {
     console.log("Clicked:", e.target);
 
     var form = $(this);
+    var idx = $('.new_vote').index(form);
     var url = $(this).attr('action');
     var method = $(this).attr('method');
     var data = $(this).serialize();
@@ -16,6 +17,7 @@ $(function() {
     console.log("Post Url:", url);
     console.log("Post Method:", method);
     console.log("Post Data:", data);
+    console.log("Index:", idx);
 
     $.ajax({
       method: method,
@@ -25,27 +27,24 @@ $(function() {
       console.log(returned_data);
 
       if(returned_data.result) {
-        // vote_count = returned_data.votes.reduce(0) { |sum, n| sum += n['value'] };
         vote_count = returned_data.votes.reduce(function(sum,n) {
           return sum + n.value;
         }, 0);
-
-
-        // var total = [0, 1, 2, 3].reduce(function(a, b) {
-        //   return a + b;
-        // });
 
         console.log("Vote Count:", vote_count);
         form.find('.btn').removeClass('gray');
         form.find('.btn').removeClass('red');
         form.find('.btn').addClass('green');
 
-        // form.parent().parent().parent().parent().parent().prev()
-        //     .find('.vote_count').text(parseInt($('.new_vote').eq(0).parent()
-        //     .parent().parent().parent().parent().prev().find('.vote_count')
-        //     .text()) + 1)
+
         form.parent().parent().parent().parent().parent().prev()
             .find('.vote_count').text(vote_count);
+
+        pairedIdx = (idx%2 == 0)? idx+1 : idx-1
+        pairedForm = $('.new_vote').eq(pairedIdx)
+        pairedForm.find('.btn').removeClass('green')
+        pairedForm.find('.btn').removeClass('red')
+        pairedForm.find('.btn').addClass('gray')
       } else {
         alert('You only have one vote per item');
       }
